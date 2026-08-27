cc7aebd2e6b3fea72ccba32088b00e856b907961
mustafayarash1-glitch <mustafa.yarash1@gmail.com>
2026-08-27 18:40:16 +0430

    Create layout.jsx

---
+++ app/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "ADEL",
+  description: "سیستم مدیریت رستورانت و شیرینی‌سرای عادل"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
